fe475f3f9edef4dfac7d2311388b4ea6909e3b9f
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-04-19 18:57:05 +0530

    Add files via upload

---
+++ Contracts/TokenICO.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DAPP is ERC20{
+    constructor() ERC20("DAPP", "@TBC"){
+        _mint(msg.sender, 1000000000000000000);
+    }
+}
